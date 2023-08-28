967c8bd3cd92b982ffd75a0753460b56095af570
tostream <benedictfwp@gmail.com>
2023-08-28 18:16:43 +0100

    Update 7.Valid Anagram.py
    update to 0(n) from o(nlogn)

--- 7.Valid Anagram.py
+++ 7.Valid Anagram.py
@@ -1,3 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        checker = {}
+        if len(s) != len(t): return False
+        for i in range(len(s)):
+            checker[s[i]] = checker.get(s[i],0)+1
+            checker[t[i]] = checker.get(t[i],0)-1
+        for j in checker:
+            if checker[j] != 0:
+                return False
+
+        return True
+#time o(n)
+#space o(n)
